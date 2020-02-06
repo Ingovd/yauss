584461b17adef41bdb3840f23a0346cec6a25638
Ingo <ingovanduijn@gmail.com>
2020-02-06 23:57:08 +0100

    Fixed minor bug in delete logic and removed pycache from git

--- yauss/app.py
+++ yauss/app.py
@@ -18,37 +18,38 @@ def my_redirect(my_key):
     return redirect("http://" + url_to_redirect.long_url)
 
 @app.route('/delete/<my_key>')
-def delete(my_key):
+def handle_delete_url(my_key):
     delete_url(my_key)
     return redirect('/')
 
-@app.route('/update/<my_key>', methods=['GET', 'POST'])
-def update(my_key):
-    if request.method == 'GET':
-        url = Url.query.get_or_404(my_key)
-        return render_template('update.html', url=url)
-    
-    elif request.method == 'POST':
-        edit_url(my_key, request.form['long_url'])
-        return redirect('/')
+@app.route('/update/<my_key>', methods=['GET'])
+def show_update_view(my_key):
+    url = Url.query.get_or_404(my_key)
+    return render_template('update.html', url=url)
+        
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'GET':
-        urls = Url.query.order_by(Url.date_created).all()
-        return render_template('index.html', urls=urls)
-    
-    elif request.method == 'POST':
-        add_url(request.form['long_url'])
-        return redirect('/')
+@app.route('/update/<my_key>', methods=['POST'])
+def handle_update_url(my_key):
+    update_url(my_key, request.form['long_url'])
+    return redirect('/')
+
+@app.route('/', methods=['GET'])
+def show_index_view():
+    urls = Url.query.order_by(Url.date_created).all()
+    return render_template('index.html', urls=urls)
+
+@app.route('/', methods=['POST'])
+def handle_add_url():
+    add_url(request.form['long_url'])
+    return redirect('/')
 
 @with_scoped_session
 def delete_url(my_key, session=None):
-    url = session.query.get_or_404(my_key)
+    url = session.query(Url).get_or_404(my_key)
     session.delete(url)
 
 @with_scoped_session
-def edit_url(my_key, long_url, session=None):
+def update_url(my_key, long_url, session=None):
     url = session.query(Url).get_or_404(my_key)
     url.long_url = long_url
 
